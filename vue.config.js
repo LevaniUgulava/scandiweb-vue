582b732e7559124c61d9060ab719c55e1e1c5062
LevaniUgulava <133241911+LevaniUgulava@users.noreply.github.com>
2024-10-25 19:33:36 +0400

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,5 @@
-const { defineConfig } = require('@vue/cli-service');
-
+const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    proxy: {
-      // This allows requests to /display to be proxied to the specified target
-      '/display': {
-        target: 'http://scandi.infinityfreeapp.com',
-        changeOrigin: true,
-        pathRewrite: { '^/display': '' }, // Remove the /display prefix when forwarding the request
-      },
-    },
-  },
-});
+  transpileDependencies: true
+})
 
