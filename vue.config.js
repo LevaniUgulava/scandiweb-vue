9f5d4007d898d98f4dad9b83a171ec8984092b8b
LevaniUgulava <133241911+LevaniUgulava@users.noreply.github.com>
2024-10-25 19:56:11 +0400

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const { defineConfig } = require('@vue/cli-service');
 
+module.exports = defineConfig({
+    transpileDependencies: true,
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://scandi.infinityfreeapp.com',
+                changeOrigin: true,
+                pathRewrite: { '^/api': '' }, // Remove the /api prefix when forwarding
+            },
+        },
+    },
+});
