c7cebe2072c874efb64e5dd087c20e7701586aea
LevaniUgulava <133241911+LevaniUgulava@users.noreply.github.com>
2024-10-26 17:21:16 +0400

    .

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://scandi.infinityfreeapp.com';
+axios.defaults.baseURL = 'http://localhost:3000/';
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 
 createApp(App).use(router).mount("#app");
