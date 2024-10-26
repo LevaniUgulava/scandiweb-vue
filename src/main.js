59785be2b07d3713d9126dcfa0422b97ce353be7
LevaniUgulava <133241911+LevaniUgulava@users.noreply.github.com>
2024-10-26 18:17:17 +0400

    .

--- src/main.js
+++ src/main.js
@@ -3,7 +3,4 @@ import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
 
-axios.defaults.baseURL = 'http://scandiweb12.000.pe/';
-axios.defaults.headers.common['Content-Type'] = 'application/json';
-
 createApp(App).use(router).mount("#app");
