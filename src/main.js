2428190948740389dbffef804f20028d0d2f067e
LevaniUgulava <133241911+LevaniUgulava@users.noreply.github.com>
2024-10-26 16:52:12 +0400

    .

--- src/main.js
+++ src/main.js
@@ -3,9 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
 
-// Configure Axios to use the proxy path
-axios.defaults.baseURL = '/api';
-axios.defaults.withCredentials = true; // Enable if your backend requires it
+axios.defaults.baseURL = 'https://scandi.infinityfreeapp.com';
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 
 createApp(App).use(router).mount("#app");
